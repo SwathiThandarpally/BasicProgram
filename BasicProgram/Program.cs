a766d7e9e2d55941b9b3adf70afc240fe23306df
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-05 21:39:47 +0530

    Added Flipping A Coin Program

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BasicProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Basic Core Problems");
+            Console.WriteLine("------------------------");
+            FlipCoin.CheckCoinFlip();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
